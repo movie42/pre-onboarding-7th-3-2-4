98dc860236334e2c4dd33d7d02d894c5499f3743
A <movie3342@gmail.com>
2022-11-12 16:33:05 +0900

    chord:스타일드 컴포넌트 글로벌 스타일, font 적용 및 tailwindcss theme 테스트
    font: 스포카 한 산스

--- preface/tailwind.config.js
+++ preface/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        layout: "13rem 1fr"
+      },
+      gridTemplateRows: {
+        basicContent: "4rem 1fr 4rem"
+      },
+      height: {
+        vh100: "100vh"
+      },
+      colors: {
+        primary1: "#091e3b",
+        primary2: "#07162c",
+        white1: "#ffffff",
+        gray1: "#c0c0c0",
+        gray2: "#333333"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
